35eafa4f142d03d4c3b3c6691c1498840ded526a
malenbarcelo <barcelomalen@gmail.com>
2025-08-18 21:30:47 -0300

    18082025 4

--- src/functions/postData.js
+++ src/functions/postData.js
@@ -75,7 +75,7 @@ function getDataToPost(createdData,sessionData) {
       sessionData.schedule.start_date.split(' ')[1],
       parseInt(sessionData.schedule.start_date.split('/')[1]),
       String(sessionData.schedule.start_day),
-      sessionData.schedule.start_date + '/' + String(sessionData.schedule.year)
+      String(sessionData.schedule.start_date + '/' + String(sessionData.schedule.year))
 
   ]
 
